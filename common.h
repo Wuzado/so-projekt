65dc9a824b101e957383f094808a9a57561e7bb9
Wuzado <git@wuzado.com>
2026-01-06 16:03:53 +0100

    Add TimeMul and SharedState struct

--- common.h
+++ common.h
@@ -1,6 +1,21 @@
 #ifndef SO_PROJEKT_COMMON_H
 #define SO_PROJEKT_COMMON_H
 
+enum class OfficeStatus {
+    Open,
+    Closed
+};
+
+struct SharedState {
+    unsigned int building_capacity;
+    unsigned int current_queue_length;
+    unsigned short int ticket_machines_num;
+    unsigned int simulated_time; // Time in seconds
+    OfficeStatus office_status;
+};
+
+constexpr int TIME_MUL = 1000;
+
 enum IDENTITY {
     PETENT,
     URZEDNIK,
