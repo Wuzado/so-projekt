7274b2e6cd7635f1cbad97067c6610444db54452
Wuzado <git@wuzado.com>
2026-01-11 12:56:19 +0100

    Rework the SharedState in preparation for simulation clock implementation

--- common.h
+++ common.h
@@ -1,6 +1,12 @@
 #ifndef SO_PROJEKT_COMMON_H
 #define SO_PROJEKT_COMMON_H
 
+#include <utility>
+
+typedef std::pair<short, short> HoursOpen; // tp, tk
+
+constexpr int TIME_MUL = 1000;
+
 enum class Identity { Petent, Urzednik, Dyrektor, Rejestracja };
 
 enum class UrzednikRole { SC, KM, ML, PD, SA };
@@ -8,21 +14,16 @@ enum class UrzednikRole { SC, KM, ML, PD, SA };
 enum class OfficeStatus { Open, Closed };
 
 struct SharedState {
+    unsigned int day;
     unsigned int building_capacity; // N
     unsigned int current_queue_length;
-    unsigned short int ticket_machines_num;
-    unsigned int simulated_time; // Time in seconds - essentially ticks (which can be affected by TIME_MUL)
+    short ticket_machines_num;
+    unsigned int simulated_time; // Essentially ticks (which can be affected by TIME_MUL)
     OfficeStatus office_status;
+
+    SharedState(unsigned int capacity) :
+        day(0), building_capacity(capacity), current_queue_length(0), ticket_machines_num(1),
+        simulated_time(0), office_status(OfficeStatus::Closed) {}
 };
-
-void init_shared_state(SharedState& state, unsigned int capacity) {
-    state.building_capacity = capacity;
-    state.current_queue_length = 0;
-    state.ticket_machines_num = 1;
-    state.simulated_time = 0;
-    state.office_status = OfficeStatus::Closed;
-}
-
-constexpr int TIME_MUL = 1000;
 
 #endif // SO_PROJEKT_COMMON_H
