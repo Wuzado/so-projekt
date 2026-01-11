0930142c9c600c5a8f0836fb67d26e21f0f6391d
Wuzado <git@wuzado.com>
2026-01-11 11:33:04 +0100

    Comments

--- common.h
+++ common.h
@@ -8,10 +8,10 @@ enum class UrzednikRole { SC, KM, ML, PD, SA };
 enum class OfficeStatus { Open, Closed };
 
 struct SharedState {
-    unsigned int building_capacity;
+    unsigned int building_capacity; // N
     unsigned int current_queue_length;
     unsigned short int ticket_machines_num;
-    unsigned int simulated_time; // Time in seconds
+    unsigned int simulated_time; // Time in seconds - essentially ticks (which can be affected by TIME_MUL)
     OfficeStatus office_status;
 };
 
@@ -19,7 +19,7 @@ void init_shared_state(SharedState& state, unsigned int capacity) {
     state.building_capacity = capacity;
     state.current_queue_length = 0;
     state.ticket_machines_num = 1;
-    state.simulated_time = 0; 
+    state.simulated_time = 0;
     state.office_status = OfficeStatus::Closed;
 }
 
